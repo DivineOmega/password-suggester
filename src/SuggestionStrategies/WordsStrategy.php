7b9938dc42ce230e50ddf10eb36b3e3545543634
Jordan Hall <jordan@hall05.co.uk>
2019-06-19 22:54:14 +0100

    Allow specification of number of words

--- src/SuggestionStrategies/WordsStrategy.php
+++ src/SuggestionStrategies/WordsStrategy.php
@@ -3,13 +3,20 @@
 namespace DivineOmega\PasswordSuggester\SuggestionStrategies;
 
 use DivineOmega\PasswordSuggester\Interfaces\SuggestionStrategyInterface;
+use InvalidArgumentException;
 
 class WordsStrategy implements SuggestionStrategyInterface
 {
+    private $numWords;
     private $words = [];
 
-    public function __construct()
+    public function __construct(int $numWords = 3)
     {
+        if ($numWords < 1) {
+            throw new InvalidArgumentException('Number of words must be at least 1.');
+        }
+
+        $this->numWords = $numWords;
         $this->words = file(__DIR__.'/../../resources/words.txt');
     }
 
@@ -17,7 +24,7 @@ class WordsStrategy implements SuggestionStrategyInterface
     {
         $password = '';
 
-        for ($i = 0; $i < 4; $i++) {
+        for ($i = 0; $i < $this->numWords; $i++) {
             $index = array_rand($this->words);
             $password .= trim($this->words[$index]).' ';
         }
